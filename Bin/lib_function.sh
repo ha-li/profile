a2ae444a9702502a982640d2c26a694ea9afc4d4
ha-li <haja.gecko@gmail.com>
2018-03-29 21:40:51 -0700

    add comment

--- Bin/lib_function.sh
+++ Bin/lib_function.sh
@@ -3,6 +3,7 @@ fingerprint() {
    ssh-keygen -E md5 -lf "$pubkeypath" | awk '{ print $2 }' | cut -c 5-
 }
 
+# aws keypairs have special format, so use this
 aws_fingerprint() {
    keypair="$1"
    openssl pkcs8 -in "$keypair" -nocrypt -topk8 -outform DER | openssl sha1 -c
